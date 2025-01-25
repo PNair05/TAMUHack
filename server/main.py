492935fe315df4081730a6675727aa8fa8c5251f
PranavUttarkar <pranavuttarkar6@gmail.com>
2025-01-25 17:25:10 -0600

    server config complete

--- server/main.py
+++ server/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, origins='*')
+
 
 @app.route('/api/users', methods=['GET'])
 def users():
